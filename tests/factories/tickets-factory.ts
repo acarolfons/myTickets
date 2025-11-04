570a687b8e89dd0ffd3d10f1cb85183c2768fe0c
Ana Carolina <carolina@MacBook-Pro-de-Ana.local>
2025-11-04 08:42:32 -0300

    chore: final changes

--- tests/factories/tickets-factory.ts
+++ tests/factories/tickets-factory.ts
@@ -3,7 +3,9 @@ import { faker } from "@faker-js/faker";
 import { createEvent } from "./event-factory";
 
 export async function createTicket(custom?: { code?: string, eventId?: number }) {
-  const event = custom?.eventId ? await prisma.event.findUnique({ where: { id: custom.eventId } }) : await createEvent();
+  const event = custom?.eventId 
+    ? await prisma.event.findUnique({ where: { id: custom.eventId } }) 
+    : await createEvent();
   const code = custom?.code ?? faker.string.alphanumeric(10);
 
   return await prisma.ticket.create({
